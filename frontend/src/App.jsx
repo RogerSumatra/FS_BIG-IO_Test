6f593a26fd4cd4999920f717ebcfafab4d7c299c
Daffarel Adyatma Winuradmaja <farelalden96@gmail.com>
2025-01-06 21:45:53 +0700

    removed unused page form route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import AddStoryPage from "./pages/AddStoryPage";
 import EditStoryPage from "./pages/EditStoryPage";
 import AddChapterPage from "./pages/AddChapterPage";
 import EditChapterPage from "./pages/EditChapterPage";
-import AddChapterWithStory from "./pages/AddChapterWithStory"
 import ViewStoryPage from "./pages/ViewStoryPage";
 import ViewChapterPage from "./pages/ViewChapterPage";
 
@@ -17,7 +16,6 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/stories" element={<AddStoryPage />} />
           <Route path="/stories/:storyid/chapter" element={<AddChapterPage/>} />
-          <Route path="/stories/stories-with-chapter" element={<AddChapterWithStory/>} />
           <Route path="/stories/:storyid/edit" element={<EditStoryPage />} />
           <Route path="/stories/:storyid/view" element={<ViewStoryPage />} />
           <Route path="/stories/:storyId/chapters/:chapterid/edit" element={<EditChapterPage />} />
